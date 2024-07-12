bd1354e0ca040643867b5c53e394ab6a4045b82d
Daffa Akhdaan <daaff12@gmail.com>
2024-07-12 18:50:40 +0700

    Code Cleaned

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,15 @@ android {
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
